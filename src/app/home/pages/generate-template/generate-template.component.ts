c052a16a456bd84cd5e1cf438423f97b1416241c
mahdieabdollahian <abdollahian73@gmail.com>
2020-07-29 12:53:22 +0430

    generate-template.component.ts: add select language and change getForm to init loginForm

--- src/app/home/pages/generate-template/generate-template.component.ts
+++ src/app/home/pages/generate-template/generate-template.component.ts
@@ -4,7 +4,7 @@ import {
   FormGroup,
   FormControl,
   FormBuilder,
-  Validators
+  Validators,
 } from "@angular/forms";
 import { LanguageService } from "./../../../core/services/language.service";
 import { ThemeService } from "./../../../core/services/theme.service";
@@ -12,27 +12,21 @@ import { ThemeService } from "./../../../core/services/theme.service";
 @Component({
   selector: "generate-template",
   templateUrl: "./generate-template.component.html",
-  styleUrls: []
+  styleUrls: [],
 })
 export class GenerateTemplateComponent implements OnInit {
   loginForm: FormGroup;
   formData;
+  selectlanguage = this.language.getLanguage();
   submitted = false;
   constructor(
     private apiService: ApiService,
     private language: LanguageService,
     private themeService: ThemeService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-    let group = {};
-    this.getForm().subscribe(data => {
-      this.formData = data.form;
-      this.formData.fields.forEach(element => {
-        group[element.name] = new FormControl("");
-      });
-      this.loginForm = new FormGroup(group);
-    });
+    this.getForm();
   }
   checkValidity(form) {
     if (form.valid) {
@@ -41,11 +35,20 @@ export class GenerateTemplateComponent implements OnInit {
     }
   }
   getForm() {
-    return this.apiService.get("auth/login").pipe();
+    this.apiService.get("auth/login").subscribe((data) => {
+      let group = {};
+      this.formData = data.form;
+      this.formData.fields.forEach((element) => {
+        group[element.name] = new FormControl("");
+      });
+      this.loginForm = new FormGroup(group);
+    });
   }
 
   changeLanguage(lang) {
     this.language.changeLanguage(lang);
     this.themeService.setDirection(lang);
+    this.selectlanguage = this.language.getLanguage();
+    this.getForm();
   }
 }
